e04b4f4218f6c3477ac75b2d8c4290060e48d07d
YourName <YourEmail@xxx.com>
2020-11-04 15:11:19 +0800

    firstcommit

---
+++ print_tree3.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+row = int(input("请输入行数："))
+for i in range(row):
+    for j in range((row - 1 - i)):
+        print(" ", end=" ")
+    for h in range(row - i,  row + i + 1):
+        print("*", end=" ")
+    for k in range(row + i + 1, 2 * row - 1):
+        print(" ", end=" ")
+    print()
